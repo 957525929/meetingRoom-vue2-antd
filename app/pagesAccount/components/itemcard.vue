ef43a6650fd95349a9bf90c0c83bc6659f29fad2
Linh <307148762@qq.com>
2021-07-27 21:48:00 +0800

    修改

--- app/pagesAccount/components/itemcard.vue
+++ app/pagesAccount/components/itemcard.vue
@@ -1,30 +1,7 @@
 <template>
 	<view>
 		<view class="commodityinfo" v-for="(item,index) in infoList" :key="index">
-			<view v-if="value==0" class="cu-item shadow">
-				<view class="cu-list menu-avatar cardtitle">
-					<view class="margin">资产类型：{{item.assetType}}</view>
-					<view class="margin flex justify-between">
-						<view :class='item.statusstyle'>{{item.statustype}}</view>
-					</view>
-				</view>
-				<view class="text-content padding-bottom cf">
-					<view class="title-wrap fl">
-						<view class="margin-left">资产名称：{{ item.deviceName }}</view>
-						<view class="margin-left">位置：{{ item.campus }}{{ item.tower }}{{item.room}}</view>
-						<view class="margin-left">数量：{{ item.amount }}</view>
-						<view class="margin-left">价格：{{ item.price }}(元)</view>
-						<view class="margin-left">厂家：{{ item.factory}}</view>
-						<view class="margin-left">购置时间：{{ item.payTime}}</view>
-					</view>
-					<view class="fr">
-						<image class="item-img" :src="item.image">
-						</image>
-					</view>
-				</view>
-			</view>
-			
-			<view v-if="item.id == value" class="cu-item shadow">
+			<view class="cu-item shadow">
 				<view class="cu-list menu-avatar cardtitle">
 					<view class="margin">资产类型：{{item.assetType}}</view>
 					<view class="margin flex justify-between">
@@ -59,6 +36,22 @@
 		data() {
 			return {
 				infoList: List,
+			}
+		},
+		
+		watch:{
+			value(){
+				if(this.value ==0){
+					this.infoList = List
+				}else{
+					let obj = []
+					for(let i =0; i< List.length; i++){
+						if(List[i].id == this.value){
+							obj.push(List[i])
+						}
+					}
+					this.infoList = obj
+				}
 			}
 		},
 
