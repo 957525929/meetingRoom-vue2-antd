9eb0edd49a2339e9613e56a9cb50b1c8854878ed
Linh <307148762@qq.com>
2021-07-30 11:20:42 +0800

    app 一些样式修改 增加消息页面和新增资产页面

---
+++ app/pages/person/manager.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-gradual-green" :isBack="true">
+			<block slot="backText"></block>
+			<block slot="content">账号管理</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
