93e01bd8f9abd6c0519da1ab0139c3d783f80322
Jiang <957525929@qq.com>
2021-08-24 18:13:36 +0800

    会议室管理增改删

---
+++ web/.history/src/views/meetingRoom/data/area_20210824135816.js
@@ -0,0 +1,12 @@
+export const areaData = [
+         {
+           value: '旗山校区',
+           label: '旗山校区',
+           children: []
+         },
+         {
+           value: '02',
+           label: '仓山校区',
+           children: []
+         }
+       ]
