f85332a10f82360f85c6eb8732583bcf95d0fb2f
SUN CHUNJIAO <578092074@qq.com>
2021-08-26 00:02:46 +0800

    会议室管理

--- web/src/views/meetingRoom/RoomInformation.vue
+++ web/src/views/meetingRoom/RoomInformation.vue
@@ -21,7 +21,7 @@
         </a-col>
         <a-col>
           <a-cascader style="width: 300px" :options="selectOptions" @click="handlePlaceTree" placeholder="请选择位置"
-            :display-render="displayRender" />
+            :display-render="displayRender"  />
         </a-col>
         <a-col :span="1"></a-col>
         <a-col>
@@ -63,7 +63,7 @@
         <a-button type="primary" icon="import">导入</a-button>
       </a-upload>
       <!--      <a-button type="primary" icon="hdd" @click="recycleBinVisible=true">回收站</a-button>-->
-      <a-dropdown v-if="selectedRowKeys.length > 0">
+      <!-- <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay" @click="handleMenuClick">
           <a-menu-item key="1">
             <a-icon type="delete" @click="batchDel" />
@@ -74,20 +74,21 @@
           批量操作
           <a-icon type="down" />
         </a-button>
-      </a-dropdown>
+      </a-dropdown> -->
     </div>
 
     <div id="dataRoomTable">
-      <a-table :dataSource="dataSource" :pagination="ipagination" rowKey="meetingroomId" @change="handleTableChange">
+      <a-table :dataSource="dataSource" :pagination="ipagination" rowKey="meetingroomId" @change="handleTableChange"
+        :loading="loading">
         <a-table-column title="#" data-index="meetingroomId" align="center" fixed="left" width="150px"></a-table-column>
         <a-table-column title="位置" data-index="placeName" align="center">
           <template slot-scope="placeName">
             <span>{{placeName.split('.')[0]}}.{{placeName.split('.')[1]}}</span>
           </template>
         </a-table-column>
-        <a-table-column title="房间号" data-index="placeName" align="center" key="room">
-          <template slot-scope="placeName">
-            <span>{{placeName.split('.')[2]}}</span>
+        <a-table-column title="房间号" align="center">
+          <template slot-scope="record">
+            <span>{{record.placeName.split('.')[2]}}</span>
           </template>
         </a-table-column>
         <a-table-column title="状态" data-index="meetingRoomState" align="center">
@@ -134,15 +135,15 @@
     <!-- 新增 -->
     <a-drawer :visible="visibleAdd" title="新增会议室" width="600px" placement="right" @close="addClose">
       <a-form-model ref="ruleForm" :model="formAdd" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-form-model-item ref="placeName" label="位置" prop="placeName" placeholder="请选择位置">
+        <a-form-model-item ref="placeName" label="位置" prop="placeName">
           <a-cascader style="width: 410px" :options="selectOptions" v-model="formAdd.placeName" placeholder="请选择位置"
-            :display-render="displayRender" @click="handlePlaceTree" />
+            :display-render="displayRenderAdd" @click="handlePlaceTree" />
         </a-form-model-item>
-        <a-form-model-item ref="room" label="房间号" prop="room">
-          <a-input v-model="room" placeholder="请输入房间号"></a-input>
+        <a-form-model-item label="房间号" ref="room" prop="room">
+          <a-input v-model="formAdd.room" placeholder="请输入房间号"></a-input>
         </a-form-model-item>
         <a-form-model-item label="状态">
-          <a-radio-group v-model="formAdd.meetingRoomState" :disabled="disabledState" @change="onChange">
+          <a-radio-group v-model="formAdd.meetingRoomState">
             <a-radio :value="0">
               空闲
             </a-radio>
@@ -158,25 +159,25 @@
           <a-input v-model="formAdd.number" placeholder="请输入房间容纳人数"></a-input>
         </a-form-model-item>
         <a-form-model-item label="基本条件" ref="condition" prop="condition">
-          <a-checkbox-group @change="onChangeCon">
+          <a-checkbox-group @change="onChangeConAdd" v-model="formAdd.condition">
             <a-row>
               <a-col :span="8">
-                <a-checkbox value="黑板" v-model="formAdd.blackboard">
+                <a-checkbox value="黑板">
                   黑板
                 </a-checkbox>
               </a-col>
               <a-col :span="8">
-                <a-checkbox value="演示电脑" v-model="formAdd.pc">
+                <a-checkbox value="演示电脑">
                   演示电脑
                 </a-checkbox>
               </a-col>
               <a-col :span="8">
-                <a-checkbox value="投影仪" v-model="formAdd.projector">
+                <a-checkbox value="投影仪">
                   投影仪
                 </a-checkbox>
               </a-col>
               <a-col :span="8">
-                <a-checkbox value="白板" v-model="formAdd.whiteboard">
+                <a-checkbox value="白板">
                   白板
                 </a-checkbox>
               </a-col>
@@ -194,13 +195,14 @@
     <a-drawer :visible="visibleModify" title="修改会议室信息" @close="modifyClose" width="600px" placement="right">
       <a-form-model ref="ruleForm" :model="formModify" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol">
         <a-form-model-item label="位置" prop="placeName">
-          <a-input v-model="formModify.placeName" disabled></a-input>
+          <a-cascader style="width: 410px" :options="selectOptions" placeholder="请选择位置"
+            :display-render="displayRenderModify" @click="handlePlaceTree" v-model="formModify.placeName" />
         </a-form-model-item>
         <a-form-model-item label="房间号" prop="room">
-          <a-input v-model="formModify.room" disabled></a-input>
+          <a-input v-model="formModify.room" placeholder="请输入房间号"></a-input>
         </a-form-model-item>
         <a-form-model-item label="状态">
-          <a-radio-group v-model="formModify.state" :disabled="disabledState" @change="onChange">
+          <a-radio-group v-model="formModify.meetingRoomState">
             <a-radio :value="0">
               空闲
             </a-radio>
@@ -216,7 +218,7 @@
           <a-input v-model="formModify.number"></a-input>
         </a-form-model-item>
         <a-form-model-item label="基本条件" prop="condition">
-          <a-checkbox-group @change="onChangeCon" v-model="formModify.condition">
+          <a-checkbox-group v-model="formModify.condition">
             <a-row>
               <a-col :span="8">
                 <a-checkbox value="黑板">
@@ -262,6 +264,9 @@
     deleteMeetingRoom,
     updateMeetingRoom
   } from '@/api/meeting'
+  import {
+    postAction
+  } from '@/api/manage'
   // import { deleteMeetingRoom } from '@/api/api'
   // import { deletePostAction } from '@/api/msg'
 
@@ -280,20 +285,13 @@
         wrapperCol: {
           span: 18
         },
-        handleFormAddData: [],
-        handleFormAddData01: [],
-        checkedValuesData01: [],
         formAdd: {
-          placeName: '',
+          placeName: [],
           number: '',
-          blackboard: '',
-          pc: '',
-          projector: '',
-          whiteboard: '',
           meetingRoomState: 0,
-          meetingroomId: ''
+          condition: [],
+          room: ''
         },
-        room:'',
         radioStyle: {
           display: 'block',
           height: '30px',
@@ -301,10 +299,10 @@
         },
         formModify: {
           number: '',
-          // placeName: '',
-          dutyTel: '',
+          placeName: [],
+          room: '',
           condition: [],
-          state: ''
+          meetingRoomState: ''
         },
         rules: {
           condition: [{
@@ -333,9 +331,8 @@
             trigger: 'blur'
           }]
         },
-        visibleDetail: false,
         // formDetail: {},
-        disabledState: false,
+        // disabledState: false,
         url: {
           list: '/MeetingRoomController/queryMeetingRoom',
           exportXlsUrl: '/sys/user/exportXls'
@@ -380,85 +377,65 @@
         return this.dataSource
       }
     },
+    watch: {
+    },
     methods: {
       displayRender({
         labels
       }) {
-        this.queryParam.placeName = labels.join('.')
-        // this.$set(this.queryParam, "placeName", labels.join('.'))
+        // this.queryParam.placeName = labels.join('.')
+       // this.$set(this.queryParam, "placeName", labels.join('.'))
         return labels.join('.')
       },
-      onChangeCon(checkedValues) {
+      displayRenderModify({
+        labels
+      }) {
+        return labels.join('.')
+      },
+      displayRenderAdd({
+        labels
+      }) {
+        return labels.join('.')
+      },
+      onChangeConAdd(checkedValues) {
         console.log('checked = ', checkedValues)
         // console.log('value = ', this.value)
-        this.checkedValuesData = checkedValues
-      },
-      onChange(e) {
-        console.log('radio checked', e.target.value)
-        this.radioRadio = e.target.value
+        // this.checkedValuesData = checkedValues
+        this.formAdd.condition = checkedValues
+
+        console.log(this.formAdd)
       },
       addRoom() {
         this.visibleAdd = true
-        // console.log(this.dataSource)
-
-        // console.log(this.handlerTable)
       },
       onSubmitAdd() {
         this.$refs.ruleForm.validate(valid => {
-          this.checkedValuesData.map(item => {
-            if (item == '白板') {
-              this.formAdd.whiteboard = 1
-            }
-            if (item == '黑板') {
-              this.formAdd.blackboard = 1
-            }
-            if (item == '投影仪') {
-              this.formAdd.projector = 1
-            }
-            if (item == '演示电脑') {
-              this.formAdd.pc = 1
-            }
-          })
-
-          this.handleFormAddData.push({
-            meetingroomId: this.ipagination.total + 1,
-            whiteboard: this.formAdd.whiteboard || 0,
-            blackboard: this.formAdd.blackboard || 0,
-            number: this.formAdd.number,
-            projector: this.formAdd.projector || 0,
-            pc: this.formAdd.pc || 0,
-            meetingRoomState: 0,
-            meetingRoomStateName: null,
-            placeName: this.formAdd.placeName.join('.'),
-            meetingRoomSize: this.formAdd.meetingRoomSize
-          })
-
           if (valid) {
-            // let length = this.dataHotel.length;
-            // this.dataHotel[length] = this.formAdd;
-            // this.dataRoom.push(this.formAdd)
-
-            console.log('11111')
-
-            addMeetingRoom(this.handleFormAddData[0])
-              .then(response => {
-                if (response.code == 200) {
-                  this.$message.success('添加成功!')
-                  this.loadData()
+            let parameter = {}
+            parameter.whiteboard = this.formAdd.condition.indexOf('白板') > -1 ? 1 : 0
+            parameter.blackboard = this.formAdd.condition.indexOf('黑板') > -1 ? 1 : 0
+            parameter.projector = this.formAdd.condition.indexOf('投影仪') > -1 ? 1 : 0
+            parameter.pc = this.formAdd.condition.indexOf('演示电脑') > -1 ? 1 : 0
+            parameter.placeName = this.formAdd.placeName.join('.') + '.' + this.formAdd.room
+            parameter.number = this.formAdd.number
+            parameter.meetingRoomState = this.formAdd.meetingRoomState
+            console.log(' parameterformAdd', parameter)
+            postAction('/MeetingRoomController/addMeetingRoom', parameter).then(res => {
+              if (res.code == 200) {
+                this.$message.success('添加成功')
+                this.loadData(1);
+                this.formAdd = {
+                  placeName: [],
+                  number: '',
+                  meetingRoomState: 0,
+                  condition: [],
+                  room: ''
                 }
-                this.formAdd = {}
-              })
-              .catch(err => {
-                console.log('报错')
-                console.log(this.handleFormAddData)
-                this.formAdd = {}
-              })
-
+              } else {
+                this.$message.warning(res.message);
+              }
+            })
             this.visibleAdd = false
-            // console.log(this.handleFormAddData)
-          } else {
-            console.log('error submit!!')
-            return false
           }
         })
       },
@@ -486,112 +463,55 @@
       Modify(record) {
         console.log(record)
         this.visibleModify = true
-        // console.log(record.condition.split(','))
-        this.handlerModify = []
-        if (record.condition.split(',')[0] == 1) {
-          this.handlerModify.push('黑板')
+        let handlerModify = []
+        if (record.pc == 1) {
+          handlerModify.push('演示电脑')
         }
-        if (record.condition.split(',')[1] == 1) {
-          this.handlerModify.push('演示电脑')
+        if (record.projector == 1) {
+          handlerModify.push('投影仪')
         }
-        if (record.condition.split(',')[2] == 1) {
-          this.handlerModify.push('投影仪')
+        if (record.whiteboard == 1) {
+          handlerModify.push('白板')
         }
-        if (record.condition.split(',')[3] == 1) {
-          this.handlerModify.push('白板')
+        if (record.blackboard == 1) {
+          handlerModify.push('黑板')
         }
-        this.formModify.room = record.room
+        this.formModify.condition = handlerModify
+        this.formModify.room = record.placeName.split('.')[2]
         this.formModify.number = record.number
-        this.formModify.placeName = record.placeName
-        this.formModify.condition = this.handlerModify
-        // console.log('121212121222222222222')
-        // console.log(record)
-        this.formModify.meetingroomId = record.meetingroomId
-        this.formModify.meetingRoomSize = record.meetingRoomSize
-        // this.formModify.dutyTel = record.dutyTel
-        this.formModify.state = record.meetingRoomState
-        console.log(this.formModify.state)
-        if (record.meetingRoomState == '1') {
-          this.disabledState = true
-        } else {
-          this.disabledState = false
-        }
-        // console.log("0109210");
-        // console.log(this.formModify.condition)
-        //this.formModify.condition = record.condition
-      },
-      ModifyDutyName(value) {
-        if (value == '李霞') {
-          this.formModify.dutyTel = '13759655332'
-        }
-        if (value == '尤晓梅') {
-          this.formModify.dutyTel = '13053955537'
-        }
-        if (value == '黄丽娟') {
-          this.formModify.dutyTel = '13659655381'
-        }
+        this.formModify.placeName = [record.placeName.split('.')[0], record.placeName.split('.')[1]]
+        this.formModify.meetingRoomState = record.meetingRoomState
+        console.log(this.formModify)
+
       },
       onSubmitModify() {
-        if (this.checkedValuesData) {
-          this.checkedValuesData.map(item => {
-            if (item == '白板') {
-              this.formModify.whiteboard = 1
-            }
-            if (item == '黑板') {
-              this.formModify.blackboard = 1
-            }
-            if (item == '投影仪') {
-              this.formModify.projector = 1
-            }
-            if (item == '演示电脑') {
-              this.formModify.pc = 1
-            }
-          })
-        }
-        console.log(this.formModify.condition)
-        this.handleFormAddData01.push({
-          meetingroomId: this.formModify.meetingroomId,
-          whiteboard: this.formModify.whiteboard || 0,
-          blackboard: this.formModify.blackboard || 0,
-          number: this.formModify.number,
-          projector: this.formModify.projector || 0,
-          pc: this.formModify.pc || 0,
-          meetingRoomState: 0,
-          meetingRoomStateName: null,
-          placeName: this.formModify.placeName,
-          meetingRoomSize: this.formModify.meetingRoomSize,
-          state: this.radioRadio
+        let parameter = {}
+        parameter.whiteboard = this.formModify.condition.indexOf('白板') > -1 ? 1 : 0
+        parameter.blackboard = this.formModify.condition.indexOf('黑板') > -1 ? 1 : 0
+        parameter.projector = this.formModify.condition.indexOf('投影仪') > -1 ? 1 : 0
+        parameter.pc = this.formModify.condition.indexOf('演示电脑') > -1 ? 1 : 0
+        parameter.placeName = this.formModify.placeName.join('.') + '.' + this.formModify.room
+        parameter.number = this.formModify.number
+        parameter.meetingRoomState = this.formModify.meetingRoomState
+        console.log(' parameterformModify', parameter)
+        postAction('/MeetingRoomController/updateMeetingRoom', parameter).then(res => {
+          if (res.code == 200) {
+            this.$message.success('修改成功')
+            this.loadData(1);
+          } else {
+            this.$message.warning(res.message);
+          }
         })
-        console.log(formModify.state)
-        console.log('852585222')
-        console.log(this.radioRadio)
-        return false
-        // console.log(this.handleFormAddData01)
-        updateMeetingRoom(this.handleFormAddData01[0])
-          .then(response => {
-            if (response.code == 200) {
-              this.$message.success('修改成功!')
-              this.loadData()
-              this.visibleModify = false
-            }
-          })
-          .catch(err => {
-            console.log('报错')
-            console.log(this.err)
-            // this.formAdd = {}
-            this.visibleModify = false
-          })
+        this.visibleModify = false
       },
       CancelModify() {
         this.visibleModify = false
       },
 
       addClose() {
-        this.$emit('close')
         this.visibleAdd = false
       },
       modifyClose() {
-        this.$emit('close')
         this.visibleModify = false
       },
       handlePlaceTree() {
@@ -661,34 +581,7 @@
         // }
         console.log(areaData)
       },
-      handlerTableData() {
-        // console.log('00000000000000000000000')
-        // console.log(this.dataSource)
-        this.dataSource.map(item => {
-          // var handlerCondition =
-          // console.log(this.handlerCondition)
-          this.handlerTable.push({
-            blackboard: item.blackboard,
-            meetingRoomSize: item.meetingRoomSize,
-            meetingRoomState: item.meetingRoomState,
-            meetingRoomStateName: item.meetingRoomStateName,
-            meetingroomId: item.meetingroomId,
-            number: item.number,
-            pc: item.pc,
-            placeName: item.placeName,
-            projector: item.projector,
-            whiteboard: item.blackboard,
-            room: item.placeName.split('.')[2],
-            condition: item.blackboard + ',' + item.pc + ',' + item.projector + ',' + item.whiteboard
-          })
-          // console.log(item)
-        })
-        console.log(this.handlerTable)
-      }
     },
-    watch: {
-      dataSource: 'handlerTableData'
-    }
   }
 </script>
 <style>
